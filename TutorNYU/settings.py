c80faf82398d56dcb3cc7a478bb2e69517af61b9
Shihui Huang <sh4480@nyu.edu>
2024-02-20 23:02:09 +0800

    homepage frontend

--- TutorNYU/settings.py
+++ TutorNYU/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'homepage',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
