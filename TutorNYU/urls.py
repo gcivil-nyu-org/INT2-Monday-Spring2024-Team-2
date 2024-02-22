f0036139765f195aaa12b5aae6424b898175b300
wuyasan <youtosan0@gmail.com>
2024-02-22 02:39:43 -0500

    Updated Tutor register with information

--- TutorNYU/urls.py
+++ TutorNYU/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('TutorRegister.url'))
+    path('register/', include('TutorRegister.url'))
 ]
