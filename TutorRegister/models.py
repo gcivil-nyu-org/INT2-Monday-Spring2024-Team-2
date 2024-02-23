d4a38edc13fe04486fdfafcc7a45a1b1a5b7cb9b
quanheng1997 <130950065+quanheng1997@users.noreply.github.com>
2024-02-22 21:30:11 -0500

    Add files via upload

--- TutorRegister/models.py
+++ TutorRegister/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Expertise(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    subject = models.CharField(max_length=255)
+    level = models.CharField(max_length=255)
+
+class Availability(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    day_of_week = models.CharField(max_length=255)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+
+class ProfileS(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    fname = models.CharField(max_length=255)
+    mname = models.CharField(max_length=255, blank=True)
+    lname = models.CharField(max_length=255)
+    gender = models.CharField(max_length=255)
+    school = models.CharField(max_length=255)
+    zip = models.CharField(max_length=255)
+    grade = models.CharField(max_length=255)
+    preferred_mode = models.CharField(max_length=255)
+    intro = models.TextField()
+
+class ProfileT(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    fname = models.CharField(max_length=255)
+    mname = models.CharField(max_length=255, blank=True)
+    lname = models.CharField(max_length=255)
+    gender = models.CharField(max_length=255)
+    school = models.CharField(max_length=255)
+    zip = models.CharField(max_length=255)
+    grade = models.CharField(max_length=255)
+    preferred_mode = models.CharField(max_length=255)
+    intro = models.TextField()
+    salary_min = models.IntegerField()
+    salary_max = models.IntegerField()
