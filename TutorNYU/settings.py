abae32c1cd38f263f3c3dd6da652b49e24e1e36e
unknown <theresatvan@gmail.com>
2024-02-24 00:17:42 -0500

    Create header and register html files

--- TutorNYU/settings.py
+++ TutorNYU/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'TutorRegister',
+    # Django crispy forms
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Crispy form template packs
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
--- TutorRegister/urls.py
+++ TutorRegister/urls.py
@@ -4,7 +4,5 @@ from . import views
 app_name = "TutorRegister"
 
 urlpatterns = [
-    path('tutor/register/', views.register_tutor, name="register_tutor"),
-    path('tutor/<int:user_id>/info/', views.tutor_info, name="tutor_info"),
-    path('student/register/', views.register_student, name="register_student")
+    path('register/', views.register, name="register")
 ]
