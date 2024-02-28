a74de9a2bc463e20cd4a04be8615e86ce617a38f
Shihui Huang <sh4480@nyu.edu>
2024-02-28 17:21:02 +0800

    fix logo issue on nav bar

--- TutorNYU/settings.py
+++ TutorNYU/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-@(8)alwdr99afwte%e4(0@@^234sllzkfoxv-osmjn*xdd!^nm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["app-env.eba-ndymim3f.us-west-2.elasticbeanstalk.com","127.0.0.1"]
 
 
 # Application definition
@@ -119,7 +120,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (
-    (BASE_DIR / 'static'),
+    os.path.join(BASE_DIR / 'static'),
+    '/static/',
+
 )
 
 # Default primary key field type
