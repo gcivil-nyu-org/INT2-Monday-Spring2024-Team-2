5986724c6e4d54b83b5e2302931a9fa531543e85
Shihui Huang <sh4480@nyu.edu>
2024-03-01 17:14:15 +0800

    black reformatting

--- TutorNYU/urls.py
+++ TutorNYU/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('TutorRegister.urls')),
-    path('verification/', include('verify_email.urls')),
-    path('', views.home, name='home'),
+    path("admin/", admin.site.urls),
+    path("auth/", include("TutorRegister.urls")),
+    path("verification/", include("verify_email.urls")),
+    path("", views.home, name="home"),
 ]
