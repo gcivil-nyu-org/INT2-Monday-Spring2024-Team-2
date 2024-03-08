04dcd7ef972813242d9b17550dc1ece9e3260e6d
Shihui Huang <sh4480@nyu.edu>
2024-03-08 23:12:47 +0800

    url configure

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = "Dashboard"
 
 urlpatterns = [
+    path("student", views.StudentDashboard, name="student_dashboard"),
+    path("tutor", views.TutorDashboard, name="tutor_dashboard"),
     path("student/edit", views.StudentInformation, name="student_edit"),
     path("tutor/edit", views.TutorInformation, name="tutor_edit"),
+    path("logout/", views.logout_view, name="logout"),
 ]
--- TutorNYU/settings.py
+++ TutorNYU/settings.py
@@ -80,22 +80,17 @@ WSGI_APPLICATION = "TutorNYU.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": os.getenv("DATABASE_NAME"),
-#         "USER": os.getenv("DATABASE_USER"),
-#         "PASSWORD": os.getenv("DATABASE_PASSWORD"),
-#         "HOST": os.getenv("DATABASE_HOST"),
-#         "PORT": os.getenv("DATABASE_PORT"),
-#     }
-# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DATABASE_NAME"),
+        "USER": os.getenv("DATABASE_USER"),
+        "PASSWORD": os.getenv("DATABASE_PASSWORD"),
+        "HOST": os.getenv("DATABASE_HOST"),
+        "PORT": os.getenv("DATABASE_PORT"),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
