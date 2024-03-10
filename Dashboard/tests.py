da39930053a5beafbc4427558fdfd0162c9de250
Shihui Huang <sh4480@nyu.edu>
2024-03-10 23:06:23 +0800

    homepage test & little styling adjustment

--- Dashboard/tests.py
+++ Dashboard/tests.py
@@ -17,3 +17,10 @@ class TutorDashboardTestCase(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "Dashboard/tutor_dashboard.html")
+
+class HomepageTestCase(TestCase):
+    def test_home_view(self):
+        url = reverse("home")
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
