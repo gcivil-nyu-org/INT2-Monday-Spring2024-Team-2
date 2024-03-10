e410c0bec4b7a021d899b08c58d749f3b3da2a60
unknown <theresatvan@gmail.com>
2024-03-10 19:52:09 -0400

    black and flake8 reformat

--- Dashboard/tests.py
+++ Dashboard/tests.py
@@ -7,43 +7,45 @@ from django.contrib.auth.models import User
 class StudentDashboardTestCase(TestCase):
     def setUp(self):
         # Create a user for testing login
-        self.user = User.objects.create_user(username='test@example.com', password='12345')
+        self.user = User.objects.create_user(
+            username="test@example.com", password="12345"
+        )
         self.client = Client()
-        
+
     def test_student_dashboard_view_with_login(self):
-        self.client.login(username='test@example.com', password='12345')
+        self.client.login(username="test@example.com", password="12345")
         url = reverse("Dashboard:student_dashboard")
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "Dashboard/student_dashboard.html")
-        
+
     def test_student_dashboard_view_without_login(self):
         url = reverse("Dashboard:student_dashboard")
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
         # Check if redirect to login page
-        self.assertTrue(response.url.startswith('/auth/login/'))
+        self.assertTrue(response.url.startswith("/auth/login/"))
 
 
 class TutorDashboardTestCase(TestCase):
     def setUp(self):
         # Create a user for testing login
-        self.user = User.objects.create_user(username='test@nyu.edu', password='12345')
+        self.user = User.objects.create_user(username="test@nyu.edu", password="12345")
         self.client = Client()
-        
+
     def test_tutor_dashboard_view_with_login(self):
-        self.client.login(username='test@nyu.edu', password='12345')
+        self.client.login(username="test@nyu.edu", password="12345")
         url = reverse("Dashboard:tutor_dashboard")
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "Dashboard/tutor_dashboard.html")
-        
+
     def test_tutor_dashboard_view_without_login(self):
         url = reverse("Dashboard:tutor_dashboard")
         response = self.client.get(url)
         self.assertEqual(response.status_code, 302)
         # Check if redirect to login page
-        self.assertTrue(response.url.startswith('/auth/login/'))
+        self.assertTrue(response.url.startswith("/auth/login/"))
 
 
 class HomepageTestCase(TestCase):
