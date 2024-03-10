ec788af54971365db1d3c894d2d6b9366055cd28
Ziyi Wang <zw3917@nyu.edu>
2024-03-10 16:29:56 -0400

    Tutor filter test modified2

--- TutorFilter/tests.py
+++ TutorFilter/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
 from TutorRegister.models import ProfileT, Expertise
-from .forms import TutorFilterForm
+from django.test import Client
 
 
 class TutorFilterTest(TestCase):
@@ -46,3 +46,34 @@ class TutorFilterTest(TestCase):
         # Check if the response context contains the expected user
         users_in_context = response.context["users"]
         self.assertTrue(any(user.user == self.testuser for user in users_in_context))
+
+    def test_filter_tutors2(self):
+        c = Client()
+        # Simulate a GET request with query parameters
+        form_data = {
+            "expertise": "Mathematics",
+            "preferred_mode": "remote",
+            "grade": "freshman",
+            "zipcode": "11111",
+            "salary_max": 20,
+        }
+        # form = TutorFilterForm(data=form_data)
+
+        response = c.post(
+            reverse("TutorFilter:filter_tutors"),
+            form_data,
+        )
+
+        # Check if the response is 200 OK
+        self.assertEqual(response.status_code, 200)
+
+        response = c.get(
+            reverse("TutorFilter:filter_tutors"),
+            form_data,
+        )
+        # Check if the response context contains the expected user
+        users_in_context = response.context["users"]
+        self.assertTrue(
+            any(user.user == self.testuser for user in users_in_context)
+        )
+        
