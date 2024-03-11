33259a4b91bde7e83ffdca9448737b23f2906747
quanheng1997 <130950065+quanheng1997@users.noreply.github.com>
2024-03-11 19:50:02 +0000

    Add form tests

--- TutorRegister/forms/test_forms.py
+++ TutorRegister/forms/test_forms.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 from TutorRegister.forms.register_login import RegisterUserForm
-
+from TutorRegister.forms.student_info import StudentForm
+from TutorRegister.models import ProfileS
 
 class RegisterUserFormTest(TestCase):
     def setUp(self):
@@ -21,8 +22,6 @@ class RegisterUserFormTest(TestCase):
             "password2": "Password123...",
         }
         form = RegisterUserForm(data=form_data)
-        if not form.is_valid():
-            print(form.errors)  # Print out the form errors for debugging
         self.assertTrue(form.is_valid())
 
     def test_form_invalid_email_exists(self):
@@ -90,3 +89,73 @@ class RegisterUserFormTest(TestCase):
             form.errors["last_name"],
             ["This field is required.", "Last name cannot be empty."],
         )
+
+class StudentFormTestCase(TestCase):
+    def test_student_form_valid_data(self):
+        form = StudentForm(data={
+            'fname': 'John',
+            'lname': 'Doe',
+            'gender': 'male',
+            'zip': '12345',
+            'school': 'ABC High School',
+            'grade': '10',
+            'preferred_mode': 'inperson',
+            'intro': 'I am a 10th-grade student.',
+        })
+        self.assertTrue(form.is_valid())
+
+    def test_student_form_invalid_data(self):
+        form = StudentForm(data={})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 8)  # Assuming all fields are required
+
+    def test_student_form_gender_choices(self):
+        form = StudentForm()
+        self.assertEqual(form.fields['gender'].choices, [
+            ("female", "Female"),
+            ("male", "Male"),
+            ("prefernottosay", "Prefer not to say"),
+        ])
+    def test_student_form_grade_choices(self):
+        form = StudentForm()
+        self.assertEqual(form.fields['grade'].choices, [
+            ("1", "1st Grade"),
+            ("2", "2nd Grade"),
+            ("3", "3rd Grade"),
+            ("4", "4th Grade"),
+            ("5", "5th Grade"),
+            ("6", "6th Grade"),
+            ("7", "7th Grade"),
+            ("8", "8th Grade"),
+            ("9", "9th Grade"),
+            ("10", "10th Grade"),
+            ("11", "11th Grade"),
+            ("12", "12th Grade"),
+            ("undergrad", "Undergraduate"),
+            ("grad", "Graduate"),
+            ("postgrad", "Post-graduate"),
+        ])
+
+    def test_student_form_preferred_mode_choices(self):
+        form = StudentForm()
+        self.assertEqual(form.fields['preferred_mode'].choices, [
+            ("inperson", "In-person"),
+            ("remote", "Remote"),
+            ("both", "Both"),
+        ])
+
+    def test_student_form_empty_fields(self):
+        form = StudentForm(data={
+            'fname': '',
+            'lname': '',
+            'gender': '',
+            'zip': '',
+            'school': '',
+            'grade': '',
+            'preferred_mode': '',
+            'intro': '',
+        })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 8)
+
+  
