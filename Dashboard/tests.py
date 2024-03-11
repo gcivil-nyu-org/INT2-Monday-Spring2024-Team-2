d292732b5a2416f538f2fe36268b2049933bdd63
Shihui Huang <sh4480@nyu.edu>
2024-03-11 21:18:52 +0800

    studentinfo & logout unit test

--- Dashboard/tests.py
+++ Dashboard/tests.py
@@ -1,6 +1,9 @@
-from django.test import TestCase, Client
+from django.test import TestCase, Client, RequestFactory
 from django.urls import reverse
 from django.contrib.auth.models import User
+from .views import StudentInformation
+from TutorRegister.models import ProfileS
+from .forms.student_info import StudentForm
 
 
 # Create your tests here.
@@ -54,3 +57,57 @@ class HomepageTestCase(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "home.html")
+
+
+class StudentInformationTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="test@example.com", password="12345"
+        )
+        self.client = Client(username="test@example.com", password="12345")
+        self.factory = RequestFactory()
+
+    def test_student_information_view(self):
+        url = reverse("Dashboard:student_profile")
+        profile, created = ProfileS.objects.get_or_create(user=self.user)
+
+        form_data = {
+            "fname": "Test",
+            "lname": "User",
+            "gender": "female",
+            "zip": "12345",
+            "school": "Test School",
+            "grade": "1",
+            "preferred_mode": "remote",
+            "intro": "Hello, this is a test.",
+        }
+
+        student_form = StudentForm(data=form_data, instance=profile)
+        request = self.factory.post(url, data=student_form.data)
+        request.user = self.user
+        response = StudentInformation(request)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(self.user.usertype.has_profile_complete)
+
+    def tearDown(self):
+        self.user.delete()
+
+
+class LogoutTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username="test@example.com", password="12345"
+        )
+        self.client = Client(username="test@example.com", password="12345")
+
+    def test_logout_view(self):
+        self.assertNotEqual(str(self.user), "AnonymousUser")
+        response = self.client.get(reverse("Dashboard:logout"))
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse("home"))
+
+        user_after_logout = response.wsgi_request.user
+        self.assertEqual(str(user_after_logout), "AnonymousUser")
+
+    def tearDown(self):
+        self.user.delete()
