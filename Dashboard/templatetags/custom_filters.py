52b65e4ce8385466481d32cbbed5c70b73141809
quanheng1997 <130950065+quanheng1997@users.noreply.github.com>
2024-03-27 16:23:21 +0000

    Attachment upload function completed

--- Dashboard/templatetags/custom_filters.py
+++ Dashboard/templatetags/custom_filters.py
@@ -6,5 +6,5 @@ register = template.Library()
 @register.filter
 def remove_prefix(value, prefix):
     if value.startswith(prefix):
-        return value[len(prefix):]
+        return value[len(prefix) :]
     return value
