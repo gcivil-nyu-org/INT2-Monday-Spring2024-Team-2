deef8381fbe3d0b44653f9944f761ec10c2506b4
Ziyi Wang <zw3917@nyu.edu>
2024-03-29 16:52:52 -0400

    Add tests for tutor filter and favorite tutors.

--- TutorFilter/tests.py
+++ TutorFilter/tests.py
@@ -8,6 +8,7 @@ from TutorRegister.models import (
     UserType,
     Availability,
     TutoringSession,
+    Favorite,
 )
 from django.test import Client
 from datetime import datetime, timedelta
@@ -35,14 +36,15 @@ class TutorFilterTest(TestCase):
         cls.testuser.usertype.save()
         cls.testuser2.usertype.save()
         Expertise.objects.create(user=cls.testuser, subject="math")
-        ProfileT.objects.create(
+
+        cls.profile_t = ProfileT.objects.create(
             user=cls.testuser,
             preferred_mode="remote",
             grade="freshman",
             zip="12345",
             salary_min=50,
         )
-        ProfileS.objects.create(
+        cls.profile_s = ProfileS.objects.create(
             user=cls.testuser2,
             school="CUNY",
             preferred_mode="remote",
@@ -61,6 +63,7 @@ class TutorFilterTest(TestCase):
             "expertise": "math",
             "zipcode": "12345",
             "salary_max": 60,
+            "rating": ">= 2 stars",
             "category": "..",
             "sortBy": "Lowest Price",
         }
@@ -70,7 +73,7 @@ class TutorFilterTest(TestCase):
         )
         self.assertEqual(response.status_code, 200)
         users_in_context = response.context["users"]
-        self.assertTrue(any(user.user == self.testuser for user in users_in_context))
+        self.assertFalse(any(user.user == self.testuser for user in users_in_context))
 
     def test_filter_tutors2(self):
         c = Client()
@@ -81,6 +84,7 @@ class TutorFilterTest(TestCase):
             "expertise": "math",
             "zipcode": "11111",
             "salary_max": 20,
+            "rating": ">= 1 star",
             "category": "..",
             "sortBy": "Highest Rating",
         }
@@ -97,6 +101,133 @@ class TutorFilterTest(TestCase):
         )
         # Check if the response context contains the expected user
         self.assertEqual(len(response2.context["users"]), 0)
+
+    def test_filter_tutors3(self):
+        c = Client()
+        c.login(username="student", password="studentpassword")
+        form_data2 = {
+            "preferred_mode": "remote",
+            "grade": "grad",
+            "expertise": "math",
+            "zipcode": "11111",
+            "salary_max": 20,
+            "rating": ">= 3 stars",
+            "category": "..",
+            "sortBy": "Highest Price",
+        }
+        # form = TutorFilterForm(data=form_data)
+
+        response2 = c.post(reverse("TutorFilter:filter_tutors"), form_data2)
+
+        # Check if the response is 200 OK
+        self.assertEqual(response2.status_code, 200)
+
+        response2 = c.get(
+            reverse("TutorFilter:filter_tutors"),
+            form_data2,
+        )
+        # Check if the response context contains the expected user
+        self.assertEqual(len(response2.context["users"]), 0)
+
+    def test_filter_tutors4(self):
+        c = Client()
+        c.login(username="student", password="studentpassword")
+        form_data2 = {
+            "preferred_mode": "remote",
+            "grade": "grad",
+            "expertise": "math",
+            "zipcode": "11111",
+            "salary_max": 20,
+            "rating": ">= 4 stars",
+            "category": "..",
+            "sortBy": "Highest Price",
+        }
+        # form = TutorFilterForm(data=form_data)
+
+        response2 = c.post(reverse("TutorFilter:filter_tutors"), form_data2)
+
+        # Check if the response is 200 OK
+        self.assertEqual(response2.status_code, 200)
+
+        response2 = c.get(
+            reverse("TutorFilter:filter_tutors"),
+            form_data2,
+        )
+        # Check if the response context contains the expected user
+        self.assertEqual(len(response2.context["users"]), 0)
+
+    def test_filter_tutors5(self):
+        c = Client()
+        c.login(username="student", password="studentpassword")
+        form_data2 = {
+            "preferred_mode": "remote",
+            "grade": "grad",
+            "expertise": "math",
+            "zipcode": "11111",
+            "salary_max": 20,
+            "rating": "= 5 stars",
+            "category": "..",
+            "sortBy": "Highest Price",
+        }
+        # form = TutorFilterForm(data=form_data)
+
+        response2 = c.post(reverse("TutorFilter:filter_tutors"), form_data2)
+
+        # Check if the response is 200 OK
+        self.assertEqual(response2.status_code, 200)
+
+        response2 = c.get(
+            reverse("TutorFilter:filter_tutors"),
+            form_data2,
+        )
+        # Check if the response context contains the expected user
+        self.assertEqual(len(response2.context["users"]), 0)
+
+    def test_add_favorite(cls):
+        # Assuming you have a URL named 'add_favorite' pointing to your add_favorite view
+        c = Client()
+        c.login(username="testuser2@example.com", password="testpassword")
+        url = reverse("TutorFilter:add_favorite")
+        data = {
+            "category_name": "Math",
+            "tutor_id": cls.profile_t.user.id,
+        }
+        response = c.post(url, data)
+
+        cls.assertEqual(response.status_code, 200)
+        cls.assertEqual(
+            response.json(),
+            {"status": "success", "message": "Add the tutor successfully!"},
+        )
+        # Check if Favorite object was indeed created
+        cls.assertTrue(
+            Favorite.objects.filter(student=cls.testuser2, tutor=cls.testuser).exists()
+        )
+
+    def test_remove_favorite(self):
+        # First, create a Favorite object to be removed
+        c = Client()
+        c.login(username="testuser2@example.com", password="testpassword")
+
+        Favorite.objects.create(
+            category="Math", student=self.testuser2, tutor=self.testuser
+        )
+
+        url = reverse("TutorFilter:remove_favorite")
+        data = {
+            "tutor_id": self.profile_t.user.id,
+        }
+        response = c.post(url, data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.json(),
+            {"status": "success", "message": "Remove the tutor successfully!"},
+        )
+        # Check if Favorite object was indeed removed
+        self.assertFalse(
+            Favorite.objects.filter(student=self.testuser, tutor=self.testuser).exists()
+        )
 
     def test_view_profile_tutor(self):
         # Test view_profile for a tutor user
