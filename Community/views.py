d0056578cfae22e55079dabebda7399c35ad7af1
unknown <theresatvan@gmail.com>
2024-04-08 03:18:48 -0400

    Refactor db operation and frontend styling

--- Community/views.py
+++ Community/views.py
@@ -3,48 +3,32 @@ from .forms import CreatePostForm, CreateReplyForm
 from django.db import IntegrityError, DatabaseError
 from django.core.paginator import Paginator
 from django.contrib import messages
+from django.db.models import Count, Prefetch
 from TutorRegister.models import Post, Reply, ProfileT, ProfileS
 
 
 def view_all_posts(request):
     userType = request.user.usertype.user_type
-    
-    posts = Post.objects.all().order_by("-post_date")
+
+    posts = (
+        Post.objects.select_related("user__usertype")
+        .prefetch_related(
+            Prefetch(
+                "post_replies",
+                queryset=Reply.objects.order_by("reply_date").select_related(
+                    "user__usertype"
+                ),
+                to_attr="ordered_replies",
+            )
+        )
+        .annotate(reply_count=Count("post_replies"))
+        .order_by("-post_date")
+    )
+
     paginator = Paginator(posts, 5)
 
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
-
-    replies = Reply.objects.all().order_by("-reply_date")
-    replied_users = set(reply.user for reply in replies)
-
-    users = set(post.user for post in posts)
-
-    user_profiles = []
-    for user in users:
-        # Check if the user is a tutor
-        if hasattr(user, "profilet"):
-            profile = ProfileT.objects.get(user=user)
-        # If not a tutor, assume it's a student
-        else:
-            profile = ProfileS.objects.get(user=user)
-        user_profiles.append(profile)
-
-    reply_count = {}
-
-    for p in posts:
-        count = replies.filter(post=p).count()
-        reply_count[p] = count
-
-    replied_profile = []
-    for user in replied_users:
-        # Check if the user is a tutor
-        if hasattr(user, "profilet"):
-            profile = ProfileT.objects.get(user=user)
-        # If not a tutor, assume it's a student
-        else:
-            profile = ProfileS.objects.get(user=user)
-        replied_profile.append(profile)
 
     if request.method == "POST":
         form = CreateReplyForm(request.POST)
@@ -66,54 +50,44 @@ def view_all_posts(request):
             else "Dashboard/base_tutor.html"
         ),
         "posts": page_obj,
-        "profiles": user_profiles,
-        "replies": replies,
-        "reply_count": reply_count,
-        "replied_profiles": replied_profile,
         "form": form,
     }
     # return render all posts
     return render(request, "posts.html", context)
 
 
-def view_post_detail(request, post_id):
-    post = get_object_or_404(Post, pk=post_id)
-    # replies = Reply.objects.filter(post=post).order_by("reply_date")
+# def view_post_detail(request, post_id):
+#     post = get_object_or_404(Post, pk=post_id)
+#     # replies = Reply.objects.filter(post=post).order_by("reply_date")
 
-    if request.method == "POST":
-        form = CreateReplyForm(request.POST)
-        if form.is_valid():
-            reply = form.save(commit=False)
-            reply.user = request.user
-            reply.post = post
-            reply.save()
+#     if request.method == "POST":
+#         form = CreateReplyForm(request.POST)
+#         if form.is_valid():
+#             reply = form.save(commit=False)
+#             reply.user = request.user
+#             reply.post = post
+#             reply.save()
 
-            return redirect("Community:all_posts")
-            # return redirect to post detail page
-        else:
-            form = CreateReplyForm()
-    return render(request, "posts.html", {"post": post, "r_form": form})
-    # render post detail page
+#             return redirect("Community:all_posts")
+#             # return redirect to post detail page
+#         else:
+#             form = CreateReplyForm()
+#     return render(request, "posts.html", {"post": post, "r_form": form})
+#     # render post detail page
 
 
 def create_post(request):
     userType = request.user.usertype.user_type
     if request.method == "POST":
         form = CreatePostForm(request.POST, request.FILES)
-        print(form)
         if form.is_valid():
-            try:
-                post = form.save(commit=False)
-                post.user = request.user
-                post.save()
-                return redirect("Community:all_posts")
-                # return redirect to post page
-            except (IntegrityError, DatabaseError) as e:
-                print(e)
-                # return render to create post page with error message
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect("Community:all_posts")
     else:
         form = CreatePostForm()
-        
+
     context = {
         "form": form,
         "baseTemplate": (
@@ -124,4 +98,3 @@ def create_post(request):
     }
 
     return render(request, "create_post.html", context)
-    # return render to create post page
