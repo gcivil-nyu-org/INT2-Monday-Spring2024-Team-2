a656bd9b7df75252ca248eab15b554a0cc44abff
unknown <theresatvan@gmail.com>
2024-04-11 14:56:48 -0400

    Add topics multiple select field to create post form

--- Community/forms.py
+++ Community/forms.py
@@ -1,12 +1,13 @@
 from django import forms
 from TutorRegister.models import Post, Reply
+from TutorRegister.presets import EXPERTISE_CHOICES
 
 
 class CreatePostForm(forms.ModelForm):
     class Meta:
         model = Post
 
-        fields = ["title", "content", "label", "attachment"]
+        fields = ["title", "content", "label", "attachment", "topics"]
 
         labels = {
             "title": "Title",
@@ -20,6 +21,7 @@ class CreatePostForm(forms.ModelForm):
             "content": forms.Textarea(attrs={"class": "form-control"}),
             "label": forms.RadioSelect(attrs={"class": "form-check-input"}),
             "attachment": forms.FileInput(attrs={"class": "form-control-file"}),
+            "topics": forms.SelectMultiple()
         }
 
     def __init__(self, *args, **kwargs):
@@ -29,6 +31,8 @@ class CreatePostForm(forms.ModelForm):
             ("resource", "Resource"),
             ("question", "Question"),
         ]
+        
+        self.fields["topics"] = forms.MultipleChoiceField(choices=EXPERTISE_CHOICES, widgets=self.Meta.widgets["topics"], required=False)
 
 
 class CreateReplyForm(forms.ModelForm):
